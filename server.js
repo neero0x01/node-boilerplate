0c93270afcc749ab3063ffa2647eac3e75a3ef56
Muhammad Ahmad <181370123@gift.edu.pk>
2021-06-10 12:57:53 +0500

    refactored

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const alert = require('awesome-cli-alerts')
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.send(`API running`)
+})
+
+app.listen(PORT, () => {
+  alert({
+    type: `success`,
+    msg: `server running on ${process.env.NODE_ENV} mode on http://localhost:${PORT}`,
+    name: `server`,
+  })
+})
